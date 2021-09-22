a94aa573bb3a057982f1296bc38a9a86970e7c43
atsushi oshima <aboutagial@yahoo.co.jp>
2021-09-22 16:04:37 +0900

    一覧画面降順テストコード作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in 'Task name', with: 'テストタスクネーム'
-        fill_in 'Content', with: 'テストコンテント'
+        fill_in '名称', with: 'テストタスクネーム'
+        fill_in '詳しい説明', with: 'テストコンテント'
         click_on "登録する"
         expect(page).to have_content '登録しました'
       end
@@ -21,6 +21,19 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task'
       end
     end
+
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しく作成されたタスクが一番上に表示される' do
+        FactoryBot.create(:task)
+        FactoryBot.create(:task_2)
+        FactoryBot.create(:task_3)
+        visit tasks_path
+        task_list = all('.task_body')
+        expect(task_list[0]).to have_content 'name_3'
+        expect(task_list[1]).to have_content 'name_2'
+        expect(task_list[2]).to have_content 'name_1'
+      end
+    end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
@@ -28,7 +41,6 @@ RSpec.describe 'タスク管理機能', type: :system do
          task = FactoryBot.create(:task)
          visit task_path(task.id)
          expect(page).to have_content 'test_task_name'
-         binding.irb
        end
      end
   end
