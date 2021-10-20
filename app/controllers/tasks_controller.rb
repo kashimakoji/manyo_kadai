4751e384318a0eaaaa2c79eae6e48a32e51c6d07
atsushi oshima <aboutagial@yahoo.co.jp>
2021-10-20 16:03:24 +0900

    エラー画面作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
     #@tasks = @tasks.where('task_name LIKE ?', "%#{params[:search]}%") if params[:search].present?
     @tasks = @tasks.status_search(params[:status]) if params[:status].present?
     @tasks = @tasks.joins(:labels).where(labels: { id: params[:label_id] }) if params[:label_id].present?
-    binding.irb
+    # binding.irb
   end
 
   def show
